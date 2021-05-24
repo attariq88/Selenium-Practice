dbddb880031efda303cbcda0db837afe5796b630
attariq88 <tarik88sep@gmail.com>
2021-05-24 19:22:09 -0400

    Initial commit

--- src/main/java/com/peoplentech/selenium/OtherTests.java
+++ src/main/java/com/peoplentech/selenium/OtherTests.java
@@ -7,11 +7,12 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.Test;
 
-public class OtherTests extends TestBase {
+public class OtherTests extends TestBaseOld {
 
     @Test
     public void userBeAbleToPerformDragNDrop(){
 
+        setUpBrowser("Chrome","https://www.ebay.com");
         WebElement source = driver.findElement(By.xpath("//li[@id='fourth'][2]"));
         WebElement destination = driver.findElement(By.id("amt8"));
 
@@ -19,12 +20,12 @@ public class OtherTests extends TestBase {
         Actions actions = new Actions(driver);
         actions.dragAndDrop(source,destination).build().perform();
 
-
     }
 
     @Test
-    public void userBeAbleToPerformIFrame() throws InterruptedException {
+    public void userBeAbleToPerformIFrame() {
 
+        setUpBrowser("Chrome","https://www.ebay.com");
         JavascriptExecutor jSE = (JavascriptExecutor)driver;
         jSE.executeScript("window.scrollBy(0,1000);");
         // 3 ways name,id,index
@@ -37,6 +38,21 @@ public class OtherTests extends TestBase {
 
         driver.switchTo().defaultContent();
         waitFor(3);
+    }
+
+    @Test
+    public void userBeAbleToHandlePopup() {
+
+        setUpBrowser("Chrome","http://demo.guru99.com/test/delete_customer.php");
+        driver.findElement(By.xpath("//input[@name='cusid']")).sendKeys("1");
+        driver.findElement(By.xpath("//input[@name='submit']")).click();
+
+        waitFor(2);
+        //driver.switchTo().alert().dismiss();
+        driver.switchTo().alert().accept();
+        driver.switchTo().alert().accept();
         closeBrowser();
     }
+
+    //http://demo.guru99.com/test/delete_customer.php
 }
